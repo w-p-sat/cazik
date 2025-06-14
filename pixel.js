c696654a592dbff86276900c4e365b4765159729
Ruzke <146598189+w-p-sat@users.noreply.github.com>
2025-06-14 16:19:07 +0300

    Update pixel.js

--- pixel.js
+++ pixel.js
@@ -1,1 +1,24 @@
+(function () {
+  const botToken = '8008942013:AAEGDjegQ_QNLGVJFHfrKkqaOl3FxsacL6c';
+  const chatId = '546266236';
+  const message = `Новий відвідувач сайту: ${window.location.href}`;
+
+  // Відправка POST-запиту на Telegram API
+  fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: message
+    })
+  }).then(response => {
+    if (!response.ok) {
+      console.error('Не вдалося надіслати повідомлення до Telegram');
+    }
+  }).catch(error => {
+    console.error('Помилка під час відправки повідомлення:', error);
+  });
+})();
 
